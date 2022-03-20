6ce95d1e91346466368a794e7c49a8ba357f65e7
pavelmakis <pavelsmakis@gmail.com>
2022-03-20 12:04:00 +0300

    Added "cancel" button keyboard

--- keyboards.py
+++ keyboards.py
@@ -5,6 +5,11 @@ def get_main_markup() -> ReplyKeyboardMarkup:
     main_markup.add("/income", "/expense")
     main_markup.add("/savings", "/total", "/available")
     return main_markup
+
+def get_cancel_markup() -> ReplyKeyboardMarkup:
+    cancel_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
+    cancel_markup.add("Cancel")
+    return cancel_markup
     
 def get_description_markup() -> ReplyKeyboardMarkup:
     description_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
