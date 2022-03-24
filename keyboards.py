5293d78a7e7a5a1b05cefd16c4bd6f79e77198a3
pavelmakis <pavelsmakis@gmail.com>
2022-03-24 16:17:27 +0300

    Added keyboard generator and 'Same amount' button

--- keyboards.py
+++ keyboards.py
@@ -3,6 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup
 def get_main_markup() -> ReplyKeyboardMarkup:
     main_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
     main_markup.add("➕Income", "➖Expense")
+    main_markup.add("💱Transaction")  
     main_markup.add("/savings", "/total", "Available")
     return main_markup
 
@@ -15,3 +16,22 @@ def get_description_markup() -> ReplyKeyboardMarkup:
     description_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
     description_markup.add("No description")
     return description_markup
+
+def get_same_amount_markup() -> ReplyKeyboardMarkup:
+    description_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
+    description_markup.add("Same amount")
+    return description_markup
+
+def get_two_row_keyboard(buttons: list) -> ReplyKeyboardMarkup:
+    buttons_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True,
+                                         one_time_keyboard=True)
+    for i in range(0, len(buttons), 2):
+        # If there is only one item left...
+        if len(buttons) - i == 1:
+            # Adding last item as big button
+            buttons_markup.add(buttons[-1])
+            break
+        # Adding items as two buttons in a row
+        buttons_markup.add(buttons[i], buttons[i+1])
+    
+    return buttons_markup
