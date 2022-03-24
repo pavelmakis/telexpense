b3ebe11c6d53585b0e22f604da8bf46d8c715238
pavelmakis <pavelsmakis@gmail.com>
2022-03-24 18:49:31 +0300

    Removed 'selective' parameter from keyboards

--- keyboards.py
+++ keyboards.py
@@ -1,30 +1,29 @@
 from aiogram.types import ReplyKeyboardMarkup
 
 def get_main_markup() -> ReplyKeyboardMarkup:
-    main_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
+    main_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     main_markup.add("➕Income", "➖Expense")
     main_markup.add("💱Transaction")  
     main_markup.add("/savings", "/total", "Available")
     return main_markup
 
 def get_cancel_markup() -> ReplyKeyboardMarkup:
-    cancel_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
+    cancel_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     cancel_markup.add("Cancel")
     return cancel_markup
     
 def get_description_markup() -> ReplyKeyboardMarkup:
-    description_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
+    description_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     description_markup.add("No description")
     return description_markup
 
 def get_same_amount_markup() -> ReplyKeyboardMarkup:
-    description_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True, one_time_keyboard=True)
+    description_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     description_markup.add("Same amount")
     return description_markup
 
 def get_two_row_keyboard(buttons: list) -> ReplyKeyboardMarkup:
-    buttons_markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True,
-                                         one_time_keyboard=True)
+    buttons_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     for i in range(0, len(buttons), 2):
         # If there is only one item left...
         if len(buttons) - i == 1:
