5d2802520f0fe8c1fa9ba69652c8d10832b43817
pavelmakis <pavelsmakis@gmail.com>
2022-03-24 18:16:51 +0300

    Added transactions form

--- forms.py
+++ forms.py
@@ -51,10 +51,10 @@ class TransactionForm(StatesGroup):
     """
     This form is used for transaction record
     """
-    outcome_account = State()
     outcome_amount = State()
-    income_account = State()
+    outcome_account = State()
     income_amount = State()
+    income_account = State()
 
 
 # --- CANCEL HANDLER ---
@@ -85,7 +85,7 @@ async def process_expense(message: types.Message, state: FSMContext):
     await ExpenseForm.amount.set()
     await bot.send_message(
             message.chat.id,
-            'Specify an amount of expense or type "cancel"',
+            'Specify an amount of expense\nor type "cancel"',
             reply_markup=keyboards.get_cancel_markup())
 
     # As the user enters the amount of spending,
@@ -206,7 +206,7 @@ async def process_income(message: types.Message, state: FSMContext):
     await IncomeForm.amount.set()
     await bot.send_message(
             message.chat.id,
-            'Specify an amount of income or type "cancel"',
+            'Specify an amount of income\nor type "cancel"',
             reply_markup=keyboards.get_cancel_markup())
 
     # As the user enters the amount of income,
@@ -376,9 +376,9 @@ async def process_record_description(message: types.Message, state: FSMContext):
         current_state = await state.get_state()
         answer_message = ""
         if "IncomeForm" in current_state:
-            answer_message = f"✅ Successfully added {data['amount']} to \n {data['category']} to {data['account']}!"
+            answer_message = f"👍 Successfully added {data['amount']} to \n {data['category']} to {data['account']}!"
         else:
-            answer_message = f"✅ Successfully added {data['amount']} to \n {data['category']} from {data['account']}!"
+            answer_message = f"👍 Successfully added {data['amount']} to \n {data['category']} from {data['account']}!"
         await bot.send_message(
             message.chat.id,
             answer_message,
@@ -390,3 +390,183 @@ async def process_record_description(message: types.Message, state: FSMContext):
     # Enter data to transactions list 
     user_sheet = sheet.Sheet()
     user_sheet.add_record(record)
+
+# --- START OF TRANSACTION HANDLERS ---
+async def process_transaction(message: types.Message, state: FSMContext):
+    """
+    The handler is used to retrieve a record of transaction through a form. 
+    To add a record, the user must specify the record data in multiple messages. 
+    To add an entry with a single command, the /addtran handler is used
+    """
+
+    # Starting form filling
+    await TransactionForm.outcome_amount.set()
+    await bot.send_message(
+            message.chat.id,
+            'Specify an amount of transaction or type "cancel"',
+            reply_markup=keyboards.get_cancel_markup())
+    
+    # As the user enters the amount of transaction,
+    # I send a query to the table to get today date and accounts
+    user_sheet = sheet.Sheet()
+    user_data = user_sheet.get_day_accounts()
+    
+    # I put the data in the state.proxy(),
+    # I have not found a better way to store the data,
+    # preserving access to it from other handlers
+    async with state.proxy() as data:
+        data['today'] = user_data['today']
+        data['accounts'] = user_data['accounts']
+
+async def process_tran_outcome_amount(message: types.Message, state: FSMContext):
+    """
+    This handler is used to get the transaction outcome amount after 
+    calling the /transaction command
+    """
+    # Parsing amount
+    parsed_amount = records.parse_outcome_amount(message.text)
+
+    # If the user entered an unrecognizable amount,
+    # stop filling out the form and send main keyboard
+    if parsed_amount is None:
+        await bot.send_message(
+            message.chat.id,
+            "❌ Cannot understand this amount...\n"
+            "Try to add /transaction one more time!",
+            reply_markup=keyboards.get_main_markup())
+        # Stop form
+        await state.finish()
+        return
+
+    # Defining keyboard markup
+    accounts_markup = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                      selective=True,
+                                                      one_time_keyboard=True)
+    async with state.proxy() as data:
+        # Write amount data to dictionary
+        data['outcome_amount'] = parsed_amount
+        # Adding buttons to markup from data get before
+        accounts_markup = keyboards.get_two_row_keyboard(data['accounts'])
+
+    # Go to the next step of form and send message
+    await TransactionForm.next()
+    await bot.send_message(
+            message.chat.id,
+            "Specify the account from which the money was transferred",
+            reply_markup=accounts_markup)
+
+async def process_outcome_account(message: types.Message, state: FSMContext):
+    """
+    This handler is used to get the outcome account after calling /transaction
+    """
+    async with state.proxy() as data:
+        # Parsing account
+        parsed_account = records._parse_account(message.text, data)
+
+        # If account is None, user entered wrong account
+        # Stop from getting and show main keyboard
+        if parsed_account == None:
+            await bot.send_message(
+                message.chat.id,
+                "❌ This account doesn't exist...\n"
+                "Try to add /transaction one more time!",
+                reply_markup=keyboards.get_main_markup())
+            # Stop form
+            await state.finish()
+            return
+
+        # If account isn't None, write account to form data
+        data['outcome_account'] = parsed_account
+
+    await TransactionForm.next()
+    # Send a message with the button for 
+    await bot.send_message(
+        message.chat.id,
+        "Specify the income amount if the amount was not transferred in full " +
+        'or in another currency.\nIf the amounts are the same, tap "Same amount".',
+        reply_markup=keyboards.get_same_amount_markup())
+
+async def process_tran_income_amount(message: types.Message, state: FSMContext):
+    """
+    This handler is used to get the transaction income amount after 
+    calling the /transaction command
+    """
+    # Defining keyboard markup
+    accounts_markup = types.ReplyKeyboardMarkup()
+
+    async with state.proxy() as data:
+        # If amount is same
+        if message.text == "Same amount":
+            data['income_amount'] = records.parse_income_amount(
+                                        str(data['outcome_amount']))
+
+        else:
+            # Parsing amount
+            parsed_amount = records.parse_income_amount(message.text)
+
+            # If the user entered an unrecognizable amount,
+            # stop filling out the form and send main keyboard
+            if parsed_amount is None:
+                await bot.send_message(
+                    message.chat.id,
+                    "❌ Cannot understand this amount...\n"
+                    "Try to add /transaction one more time!",
+                    reply_markup=keyboards.get_main_markup())
+                # Stop form
+                await state.finish()
+                return
+            
+            # Adding parsed data to form data
+            data['income_amount'] = parsed_amount
+        
+        # Forming two column markup from data get before
+        accounts_markup = keyboards.get_two_row_keyboard(data['accounts'])
+
+    # Go to the next step of form and send message
+    await TransactionForm.next()
+    await bot.send_message(
+            message.chat.id,
+            "Specify the account to which\nthe money is transferred",
+            reply_markup=accounts_markup)
+
+async def process_income_account(message: types.Message, state: FSMContext):
+    """
+    This handler is used to get the income account after calling /transaction
+    """
+    transaction_record = []
+    async with state.proxy() as data:
+        # Parsing account
+        parsed_account = records._parse_account(message.text, data)
+
+        # If account is None, user entered wrong account
+        # Stop from getting and show main keyboard
+        if parsed_account == None:
+            await bot.send_message(
+                message.chat.id,
+                "❌ This account doesn't exist...\n"
+                "Try to add /transaction one more time!",
+                reply_markup=keyboards.get_main_markup())
+            # Stop form
+            await state.finish()
+            return
+
+        # If account isn't None, write account to form data
+        data['income_account'] = parsed_account
+        # Prepare transaction record
+        transaction_record = [data['today'], data['outcome_amount'],
+                              data['outcome_account'], 
+                              data['income_amount'], data['income_account']]
+
+    # Stop form filling
+    await state.finish()
+
+    # Enter data to transactions list 
+    user_sheet = sheet.Sheet()
+    user_sheet.add_transaction(transaction_record)
+
+    # Send a message with the button for 
+    await bot.send_message(
+        message.chat.id,
+        "👍 Successfully added transaction\n" +
+        f"from {transaction_record[2]} to {transaction_record[4]}!",
+        reply_markup=keyboards.get_main_markup())
