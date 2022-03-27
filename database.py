6a385c885d6a730e582c122843da0cc44df16bd9
pavelmakis <pavelsmakis@gmail.com>
2022-03-27 09:40:47 +0300

    Comments

--- database.py
+++ database.py
@@ -1,3 +1,7 @@
+"""
+This file is used for integrate with database where
+user sheet URL's are stored.
+"""
 import os
 import sqlite3
 
@@ -5,17 +9,20 @@ conn = sqlite3.connect(os.path.join("db", "user_sheets.db"))
 cursor = conn.cursor()
 
 def get_sheet_url(user_id: str):
-    cursor.executemany(
-    'SELECT * FROM "user_sheets" '
-    f"WHERE user_id={user_id}")
-    conn.commit()
+    """Get users sheet URL by Telegram id"""
+    cursor.executemany(f'''
+    SELECT * FROM user_sheets
+    WHERE user_id={user_id}
+    ''')
     return cursor.fetchone()
 
 def insert_sheet_url(user_id: str, sheet_url: str):
-    cursor.execute(
-    "INSERT INTO user_sheets ("
-    "    user_id, sheet_url"
-    f") VALUES ('{user_id}', '{sheet_url}')")
+    """Add users sheet URL to database"""
+    cursor.execute(f'''
+    INSERT INTO user_sheets (
+        user_id, sheet_url"
+    ) VALUES ('{user_id}', '{sheet_url}')
+    ''')
     conn.commit()
 
 def init_if_not_exists():
