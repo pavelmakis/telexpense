9e2048a7b705317667369daaf70a13b4cd4c3ee2
pavelmakis <pavelsmakis@gmail.com>
2022-04-11 19:26:07 +0300

    Added /donate command handler, connected payment provider

--- server.py
+++ server.py
@@ -10,9 +10,12 @@ from sheet import Sheet
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types.message import ContentType
 
 
 API_TOKEN = os.getenv('TELEXPENSE_TOKEN')
+PROVIDER_TOKEN = os.getenv('TELEXPENSE_PROVIDER_TOKEN')
+PRICE = [types.LabeledPrice(label="Donate", amount=300)]
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -373,6 +376,45 @@ async def add_tran(message: types.Message):
         "👍 Successfully added transaction from \n" +
         f"{parsed_transaction[2]} to {parsed_transaction[4]}!")
 
+@dp.message_handler(commands=['donate'])
+async def send_invoice(message: types.Message):
+    # Send help message 
+    await bot.send_message(
+        message.chat.id,
+        answers.donate_mes,
+        reply_markup=keyboards.get_main_markup()
+    )
+    # Send invoice
+    await bot.send_invoice(
+        message.chat.id,
+        title="Donation to developer",
+        description=answers.donate_description,
+        provider_token=PROVIDER_TOKEN,
+        currency='eur',
+        is_flexible=False,
+        prices=PRICE,
+        max_tip_amount=9700,
+        suggested_tip_amounts=[200, 700, 1200, 1700],
+        payload='Donate invoice sent'
+    )
+
+# I have a simple donate button, so I answer OK to query
+@dp.pre_checkout_query_handler(lambda query: True)
+async def process_pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
+    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
+
+@dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
+async def process_successful_payment(message: types.Message):
+    """Sends thanks message if successfull payment"""
+    await bot.send_message(
+        message.chat.id,
+        answers.successfull_payment.format(
+            total_amount=message.successful_payment.total_amount // 100,
+            currency=message.successful_payment.currency
+        ),
+        parse_mode='Markdown',
+        reply_markup=keyboards.get_main_markup()
+    )
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
