ed630dd9b9826455cbae8e506fd40d2845414dab
pavelmakis <pavelsmakis@gmail.com>
2022-05-30 13:28:03 +0300

    Moved all donations handlers to separate file

--- handlers/donations.py
+++ handlers/donations.py
@@ -1,7 +1,10 @@
 import os
 
 from aiogram import Dispatcher
-from aiogram.types import LabeledPrice, Message
+from aiogram.types import LabeledPrice, Message, PreCheckoutQuery
+from aiogram.types.message import ContentType
+
+from server import bot
 
 PROVIDER_TOKEN = os.getenv("TELEXPENSE_PROVIDER_TOKEN")
 PRICE = [LabeledPrice(label="Donate", amount=300)]
@@ -10,11 +13,29 @@ import messages
 from keyboards import get_main_markup
 
 
+# I have a simple donate button, so I answer OK to query
+async def process_pre_checkout_query(pre_checkout_query: PreCheckoutQuery):
+    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
+
+
+async def process_successful_payment(message: Message):
+    """Sends thanks message if successfull payment"""
+    await bot.send_message(
+        message.chat.id,
+        messages.successfull_payment.format(
+            total_amount=message.successful_payment.total_amount // 100,
+            currency=message.successful_payment.currency,
+        ),
+        parse_mode="Markdown",
+        reply_markup=get_main_markup(),
+    )
+
+
 async def send_invoice(message: Message):
     # Send help message
     await message.answer(messages.donate_mes, reply_markup=get_main_markup())
     # Send invoice
-    await message.answer(
+    await bot.send_invoice(
         message.chat.id,
         title="Donation to developer",
         description=messages.donate_description,
@@ -28,5 +49,11 @@ async def send_invoice(message: Message):
     )
 
 
-def register_user(dp: Dispatcher):
+def register_donations(dp: Dispatcher):
     dp.register_message_handler(send_invoice, commands=["donate"])
+    dp.register_pre_checkout_query_handler(
+        process_pre_checkout_query, lambda query: True
+    )
+    dp.register_message_handler(
+        process_successful_payment, content_types=ContentType.SUCCESSFUL_PAYMENT
+    )
