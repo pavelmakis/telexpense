a4b109390baeb25c416c652dfe6727909aa9f7d4
pavelmakis <pavelsmakis@gmail.com>
2022-06-12 13:37:42 +0300

    Added multi-language sending

--- handlers/admin.py
+++ handlers/admin.py
@@ -4,35 +4,70 @@ from asyncio import sleep
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from aiogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 
 import database
+from keyboards.user import main_keyb
 from server import bot
 
 admin_id = int(os.getenv("ADMIN_ID"))
 
 
 class Mailing(StatesGroup):
+    language = State()
     text = State()
     start = State()
 
 
-async def mailing(message: types.Message):
-    await message.answer("Пришлите текст сообщения для рассылки")
+async def mailing(message: Message):
+    markup = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton("Английский", callback_data="send_en")],
+            [InlineKeyboardButton("Русский", callback_data="send_ru")],
+            [InlineKeyboardButton("Отмена", callback_data="cancel")],
+        ]
+    )
+
+    await message.answer("На каком языке будет рассылка?", reply_markup=markup)
+
+    await Mailing.language.set()
+
+
+async def mailing_lang(call: CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(call.id)
+
+    if call.data == "cancel":
+        await state.reset_state()
+        await state.finish()
+
+        await bot.send_message(
+            call.from_user.id,
+            "Отменено",
+            reply_markup=main_keyb(),
+        )
+
+        await bot.delete_message(call.from_user.id, call.message.message_id)
+
+        return
+
+    await state.update_data(lang=call.data[-2:])
+
+    await call.message.answer("Пришлите текст сообщения для рассылки")
+
     await Mailing.text.set()
 
 
 async def mailing_text(message: types.Message, state: FSMContext):
     text = message.text
     await state.update_data(text=text)
-    # markup = InlineKeyboardMarkup(
-    #     inline_keyboard=
-    #     [
-    #         [InlineKeyboardButton(text="Русский", callback_data="ru")],
-    #         [InlineKeyboardButton(text="English", callback_data="en")],
-    #         [InlineKeyboardButton(text="Україньска", callback_data="uk")],
-    #     ]
-    # )
+
+    async with state.proxy() as data:
+        lang = data["lang"]
 
     markup = InlineKeyboardMarkup(
         inline_keyboard=[
@@ -43,16 +78,10 @@ async def mailing_text(message: types.Message, state: FSMContext):
 
     await bot.send_message(
         message.from_user.id,
-        f"Сообщение будет выглядеть так:\n\n{text}",
+        f"Сообщение для пользователей с языком {lang}:\n\n{text}",
         parse_mode="Markdown",
         reply_markup=markup,
     )
-
-    # await message.answer("Пользователям на каком языке разослать это сообщение?\n\n"
-    #                        "Текст:\n"
-    #                        "{text}").format(text=text),
-    #                      reply_markup=markup
-    # await Mailing.Language.set()
 
     await Mailing.start.set()
 
@@ -63,17 +92,19 @@ async def mailing_start(call: types.CallbackQuery, state: FSMContext):
         # await state.finish()
 
         await bot.edit_message_text(
-            "OK, cancelled!",
+            "Отменено",
             call.from_user.id,
             call.message.message_id,
         )
     else:
         data = await state.get_data()
         text = data.get("text")
+        lang = data.get("lang")
+
         await state.reset_state()
         await call.message.edit_reply_markup()
 
-        users = database.get_all_users()
+        users = database.get_users_by_language(str(lang))
         sent_counter, del_counter = 0, 0
 
         for user in users:
@@ -83,27 +114,34 @@ async def mailing_start(call: types.CallbackQuery, state: FSMContext):
                 )
 
                 await sleep(0.3)
-            except Exception:
-                # database.delete_sheet_id()
+            except Exception as e:
+                print(e)
+                # database.delete_sheet_id(user)
                 del_counter += 1
                 continue
             sent_counter += 1
+
         await call.message.answer(
             "Рассылка завершена.\n\n"
             f"Сообщение отправлено {sent_counter} раз, "
-            f"{del_counter} пользователей удалено."
+            f"{del_counter} пользователей удалено.",
+            reply_markup=main_keyb(),
         )
+
 
 async def count_users(message: Message):
     users = database.get_user_count()
-
     await message.answer(f"Пользователей в базе: {users}")
 
 
 def register_admin(dp: Dispatcher):
     dp.register_message_handler(mailing, user_id=admin_id, commands=["sendall"])
+    dp.register_callback_query_handler(
+        mailing_lang, user_id=admin_id, state=Mailing.language
+    )
     dp.register_message_handler(mailing_text, user_id=admin_id, state=Mailing.text)
     dp.register_callback_query_handler(
         mailing_start, user_id=admin_id, state=Mailing.start
     )
+
     dp.register_message_handler(count_users, user_id=admin_id, commands=["countusers"])
