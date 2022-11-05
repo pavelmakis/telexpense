adafa69c6e18eee7a605185cacd0f18c4299e93e
pavelmakis <pavelsmakis@gmail.com>
2022-11-05 10:46:30 +0300

    update: Added bot initialization

---
+++ bot.py
@@ -0,0 +1,108 @@
+import asyncio
+import logging
+
+# import sentry_sdk
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from aiogram.dispatcher.webhook import get_new_configured_app
+from aiohttp import web
+from aiohttp.web import Application
+
+# from sentry_sdk.integrations.aiohttp import AioHttpIntegration
+# from sentry_sdk.integrations.logging import LoggingIntegration
+
+import config
+
+# import db
+
+# if not config.DEBUG:
+#     sentry_sdk.init(
+#         dsn=config.SENTRY_DSN,
+#         environment=config.ENVIRONMENT,
+#         traces_sample_rate=0.1,
+#         integrations=[
+#             AioHttpIntegration(),
+#             LoggingIntegration(level=logging.INFO, event_level=logging.ERROR),
+#         ],
+#     )
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] : #%(levelname)-8s - %(filename)s:%(lineno)d - %(name)s - %(message)s",
+)
+
+
+def register_all_middlewares(dp: Dispatcher) -> None:
+    pass
+
+
+def register_all_filters(dp: Dispatcher) -> None:
+    # dp.bind_filter(IsAdmin)
+    pass
+
+
+def register_all_handlers(dp: Dispatcher) -> None:
+    register_start(dp)
+    pass
+
+
+async def on_startup(app: Application) -> None:
+    bot_info = await bot.get_me()
+    logging.info(f"🤙 Hello from {bot_info.full_name} [@{bot_info.username}]!")
+
+    # Setting webhook URL
+    await bot.set_webhook(url=config.WEBHOOK_URL)
+    logging.info(f"Webhook was set on '{config.WEBHOOK_URL}'")
+
+    # Openning connection pool
+    # await db.open_pool()
+
+    # await register_all_handlers(dp)
+
+
+async def on_shutdown(app: Application) -> None:
+    # Disconecting redis storage
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    # Closing connection pool
+    # await db.close_pool()
+    pass
+
+
+# Initialize storage
+storage = RedisStorage2(
+    host=config.REDIS_STORAGE_HOST,
+    port=config.REDIS_STORAGE_PORT,
+    db=config.REDIS_STORAGE_DB,
+    prefix="telexpense_fsm",
+)
+
+
+# Initialize bot and dispatcher
+bot = Bot(token=config.API_TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot, storage=storage)
+
+
+if __name__ == "__main__":
+    # from core.filters.isadmin import IsAdmin
+    from telexpense.handlers.start import register_start
+
+    register_all_middlewares(dp)
+    register_all_filters(dp)
+    register_all_handlers(dp)
+
+    bot_app = get_new_configured_app(dispatcher=dp, path=config.WEBHOOK_PATH)
+    bot_app.on_startup.append(on_startup)
+    bot_app.on_shutdown.append(on_shutdown)
+
+    # Adding route for handling payment requests
+    # app.add_routes([web.post("/{project_name}/prodamus", prodamus)])
+    # logging.info("Route for payments added on '/{project_name}/prodamus'")
+
+    web.run_app(
+        app=bot_app,
+        host=config.WEBAPP_HOST,
+        port=config.WEBAPP_PORT,
+    )
