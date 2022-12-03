8f09baa13aa8312db307b043ab685cfc0ae18df3
pavelmakis <pavelsmakis@gmail.com>
2022-12-03 21:02:19 +0300

    update: Added class for managing pool connections

--- db/__init__.py
+++ db/__init__.py
@@ -1,18 +1,25 @@
-from asyncpg import create_pool
-from config import DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT
+from asyncpg import Pool, create_pool
+from asyncpg.pool import PoolAcquireContext
+
+from config import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
 
 
-async def open_pool() -> None:
-    global pool
+class PostgresConnector:
+    def __init__(self) -> None:
+        self.pool: Pool = None
 
-    pool = await create_pool(
-        dsn=f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
-        min_size=50,
-        max_size=1000,
-        timeout=10,
-        max_inactive_connection_lifetime=10,
-    )
+    async def open_pool(self) -> None:
+        self.pool = await create_pool(
+            dsn=f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
+            min_size=50,
+            max_size=100,
+            timeout=10,
+            max_inactive_connection_lifetime=10,
+        )
 
+    async def acquire(self) -> PoolAcquireContext:
+        return self.pool.acquire()
 
-async def close_pool() -> None:
-    await pool.close()
+    async def close_pool(self) -> None:
+        if self.pool is not None:
+            await self.pool.close()
